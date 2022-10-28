59703e8d9273718e6e728a69b13c6bb49c9e0653
Christoph Purrer <christophpurrer@gmail.com>
2022-10-28 11:21:37 +0200

    Add replacement patterns ...

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,18 @@ import path from 'path';
 
 function fabricateFile(file: string) {
   let content = fs.readFileSync(file, {encoding: 'utf8'});
-  content = content.replace(
+  content = content.replaceAll(
     '#import <UIKit/UIKit.h>',
     '#import <React/RCTUIKit.h> // TODO(macOS GH#774)',
   );
-  content = content.replace('UIColor ', 'RCTUIColor ');
+  content = content.replaceAll(' UIColor ', ' RCTUIColor ');
+  content = content.replaceAll('[UIColor ', '[RCTUIColor ');
+  content = content.replaceAll('(UIColor ', '(RCTUIColor ');
+  content = content.replaceAll('(UIView<', '(RCTUIView<');
+  content = content.replaceAll(
+    '#if TARGET_OS_MAC && TARGET_OS_IPHONE',
+    '#if TARGET_OS_MAC && (TARGET_OS_IPHONE || TARGET_OS_OSX)',
+  );
   fs.writeFileSync(file, content);
 }
 
@@ -19,6 +26,8 @@ function processDirectory(react_native_macos_path: string) {
   console.log(`processDirectory :${react_native_macos_path}`);
   [
     'ReactCommon/react/renderer/graphics/platform/ios/RCTPlatformColorUtils.mm',
+    'React/Fabric/Mounting/RCTComponentViewProtocol.h',
+    'ReactCommon/react/renderer/textlayoutmanager/platform/ios/RCTTextLayoutManager.h',
   ].forEach((file) => {
     const filePath = path.join(react_native_macos_path, file);
     fabricateFile(filePath);
